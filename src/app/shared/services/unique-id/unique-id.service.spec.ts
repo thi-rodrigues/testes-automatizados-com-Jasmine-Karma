72b34510b33ff46093399e621b7ddcce65dee39f
thi-rodrigues <49729506+thi-rodrigues@users.noreply.github.com>
2024-06-19 22:24:40 -0300

    test gerar unico ids

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -2,9 +2,19 @@ import { UniqueIdsService } from "./unique-id.service";
 
 describe(UniqueIdsService.name, () => {
 
-  it(`${UniqueIdsService.prototype.generatedUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+  it(`#${UniqueIdsService.prototype.generatedUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdsService();
     const id = service.generatedUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdsService.prototype.generatedUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    const service = new UniqueIdsService();
+    const ids = new Set();
+
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generatedUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
   });
 });
