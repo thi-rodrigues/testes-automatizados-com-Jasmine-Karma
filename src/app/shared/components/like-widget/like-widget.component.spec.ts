ccd9477db2977c2b34ff9908a5f06c10292922ec
thi-rodrigues <49729506+thi-rodrigues@users.noreply.github.com>
2024-06-21 16:58:04 -0300

    refatorando  usando o import geral com o AppModule

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -3,15 +3,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
 import { UniqueIdsService } from '../../services/unique-id/unique-id.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppModule } from 'src/app/app.module';
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent],
-      providers: [UniqueIdsService],
-      imports: [FontAwesomeModule]
+      imports: [AppModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
