4aa7818a80c4e5865087ebaf6a02dd980d4c324f
thi-rodrigues <49729506+thi-rodrigues@users.noreply.github.com>
2024-06-24 21:30:35 -0300

    testando propriedade output

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from 'src/app/app.module';
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,24 +13,51 @@ describe(LikeWidgetComponent.name, () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
   });
 
   it('Should create component', () => {
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 
   it('Should auto generate ID when id input property is missing', () => {
-    const component = fixture.componentInstance;
     fixture.detectChanges(); // faz a chamanda ao component, e o ngOnit é chamado
     expect(component.id).toBeTruthy();
   });
 
   it('Should NOT auto generate ID when id input property is present', () => {
-    const component = fixture.componentInstance;
     const someId = 'someId';
     component.id = someId;
     fixture.detectChanges();
     expect(component.id).toBe(someId);
   });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    Ex 1- should trigger emission when called`, (done) => {
+      fixture.detectChanges();
+      component.liked.subscribe(() => {
+        expect(true).toBeTrue();
+        done();
+        });
+      component.like();
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    Ex 1- should trigger emission when called`, (done) => {
+      fixture.detectChanges();
+      component.liked.subscribe(() => {
+        expect(true).toBeTrue();
+        done();
+        });
+      component.like();
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    Ex 2- should trigger emission when called`, () => {
+      spyOn(component.liked, 'emit');
+      fixture.autoDetectChanges();
+      component.like();
+      expect(component.liked.emit).toHaveBeenCalled();
+  });
+
 });
